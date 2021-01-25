c025e1aa123596963d443c27ec3fb2007face87a
ObulReddyL <77969745+ObulReddyL@users.noreply.github.com>
2021-01-25 20:16:24 +0530

    Add files via upload

---
+++ ShopBridgeAPI/ShopBridgeAPI/Configuration/WebAPIConfig.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Owin;
+using ShopBridgeAPI.DataAccess;
+using System.Reflection;
+using System.Web.Http;
+
+namespace ShopBridgeAPI.Configuration
+{
+    public static class WebAPIConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.EnableCors();
+
+
+            //var builder = new ContainerBuilder();
+            //builder.RegisterApiControllers(Assembly.GetExecutingAssembly(), typeof(Controller.ItemController).Assembly);
+            //builder.RegisterType<Model1Container>().As<IModel1Container>().InstancePerRequest();
+
+            //var container = builder.Build();
+            ////app.UseAutofacMiddleware(container);
+            //app.UseAutofacMiddleware(container);
+
+            //GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container); //Set the WebApi DependencyResolver
+
+        }
+    }
+}
